bd3c1e83fd27e64baaa2d6042bec23e0e502c4ee
Julio Briones <Julio_Briones@epam.com>
2022-03-17 19:25:30 -0600

    Added decouple for env vars.

--- wjApp/settings.py
+++ wjApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8azc5fr8yf%3zjui-22x77k7k(dtze4gsnx)txw0(lv^l2p&l$"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
